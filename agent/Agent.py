66184fa8e71499105dfc1a4173ce07d63a95500d
MyronHelium <rafaelhe661@outlook.com>
2025-01-21 10:20:41 +0800

    reward try 5

--- agent/Agent.py
+++ agent/Agent.py
@@ -335,8 +335,7 @@ class EnvRemoteArray:
             states = ray.get([worker.get_current_state.remote() for worker in self.workers])
         else:
             states = [worker.get_current_state() for worker in self.workers]
-        print("sample1step")
-        reward = torch.zeros(self.worker_num, 1).to(device)
+        rewards = torch.zeros(1).to(device)
 
         states = np.array(states)
         with torch.no_grad():
@@ -344,7 +343,7 @@ class EnvRemoteArray:
                 actions = [self.env.normalization(self.action_space.sample()) for item in states]
             else:
                 states_tensor = torch.from_numpy(states).to(torch.get_default_dtype()).to(device).unsqueeze(1)
-                actions = cur_policy.inference_one_step(states_tensor, reward, self.deterministic).to(torch.device('cpu')).squeeze(1).numpy()
+                actions = cur_policy.inference_one_step(states_tensor, rewards, self.deterministic).to(torch.device('cpu')).squeeze(1).numpy()
 
         if self.use_remote:
             srd = ray.get([worker.step.remote(action, env_ind) for action, worker in zip(actions, self.workers)])
@@ -389,7 +388,6 @@ class EnvRemoteArray:
         cur_policy = policy
         worker = self.workers[0]
         state = worker.get_current_state()
-        print("sample1step1env")
         reward = torch.zeros(1).to(device)
         action = self.get_action(state, cur_policy, reward, random, device)
         if need_info:
